ff663ba4766651eed28f9198847f88329fb92136
TubagusWiranata <65150159+TubagusWiranata@users.noreply.github.com>
2020-05-11 05:03:57 -0700

    Add files via upload
    Buatlah program array satu dimensi yang menampilkan NIM masing-masing mahasiswa

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+main (){
+char* my_array[15]={"01","02","03","04","05","06","07","08","09","10","11","12","13","14","15"};
+	
+	cout<<"nim mahasiswa ke- 1 = "<<my_array[0]<<"\n";
+	cout<<"nim mahasiswa ke- 2 = "<<my_array[1]<<"\n";
+	cout<<"nim mahasiswa ke- 3 = "<<my_array[2]<<"\n";
+	cout<<"nim mahasiswa ke- 4 = "<<my_array[3]<<"\n";
+	cout<<"nim mahasiswa ke- 5 = "<<my_array[4]<<"\n";
+	cout<<"nim mahasiswa ke- 6 = "<<my_array[5]<<"\n";
+	cout<<"nim mahasiswa ke- 7 = "<<my_array[6]<<"\n";
+	cout<<"nim mahasiswa ke- 8 = "<<my_array[7]<<"\n";
+	cout<<"nim mahasiswa ke- 9 = "<<my_array[8]<<"\n";
+	cout<<"nim mahasiswa ke- 10 = "<<my_array[9]<<"\n";
+	cout<<"nim mahasiswa ke- 11 = "<<my_array[10]<<"\n";
+	cout<<"nim mahasiswa ke- 12 = "<<my_array[11]<<"\n";
+	cout<<"nim mahasiswa ke- 13 = "<<my_array[12]<<"\n";
+	cout<<"nim mahasiswa ke- 14 = "<<my_array[13]<<"\n";
+	cout<<"nim mahasiswa ke- 15 = "<<my_array[14]<<"\n";
+
+}
