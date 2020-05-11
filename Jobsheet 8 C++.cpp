c150cebdc8500560a55f45bf5028e4c9525ecae3
TubagusWiranata <65150159+TubagusWiranata@users.noreply.github.com>
2020-05-11 05:27:11 -0700

    Add files via upload
    Buatlah program mengisi nilai Array dan menampilkannya dengan perulangan dengan indeks Array dan batas pengulangan menggunakan NIM terakhir setiap mahasiswa

---
+++ Jobsheet 8 C++.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+int x[5],i;
+cout<<"NIM : F1B019065\n";
+for(i=0;i<=1;i++)
+{
+cout<<"Masukkan nim "<<i+1<<" : ";
+cin>>x[i];
+}
+cout<<endl;
+for(i=0;i<=1;i++)
+{
+cout<<"Nim ke "<<i+1<<" = "<<x[i]<<endl;
+} cout<<endl;
+
+return 0;
+}
+
